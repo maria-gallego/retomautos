c6b72977d2c6a5155cf3368000a5f9db97a3e5f7
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2020-03-22 19:12:52 +1100

    home page finished

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,9 @@ class VisitorsController < ApplicationController
   def home
   end
 
+  def financing
+  end
+
   def about_us
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'visitors#home'
+  get '/financiacion', to: 'visitors#financing'
   get '/sobre-nosotros', to: 'visitors#about_us'
 end
