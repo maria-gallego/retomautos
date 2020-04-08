d4fa2e6c2040a1117dfff10a0389844ca45f3f5e
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2020-04-08 22:57:33 +1000

    Add  google analytic id per environment

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 module GoogleAnalytics
-  TRACKING_ID = Rails.application.credentials.google_analytics_id
+  TRACKING_ID = Rails.application.credentials.dig Rails.env.to_sym, :google_analytics_id
   JS_URL = "https://www.googletagmanager.com/gtag/js?id=#{GoogleAnalytics::TRACKING_ID}"
 end
