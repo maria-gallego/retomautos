6f71890062b23c0cfbc49bbcf49774ed7153bc4a
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2020-05-14 22:15:46 +1000

    Flash fixed. Required fixed. Email to pipe.

--- app/mailers/find_car_request_mailer.rb
+++ app/mailers/find_car_request_mailer.rb
@@ -1,15 +1,15 @@
 class FindCarRequestMailer < ApplicationMailer
-  SENDGRID_VERIFIED_SENDER = 'mariapaulagallegoc@gmail.com'
+  SENDGRID_VERIFIED_SENDER = 'felipe.gallego@retomautos.com'
   default from: SENDGRID_VERIFIED_SENDER
   #default from: OWNER_EMAIL
 
   def staff_email(find_car_request)
     @find_car_request = find_car_request
-    mail(to: SENDGRID_VERIFIED_SENDER, from: SENDGRID_VERIFIED_SENDER, subject: "Solicitud de carro")
+    mail(to: [SENDGRID_VERIFIED_SENDER, 'mariapaulagallegoc@gmail.com'], subject: "Solicitud de carro")
   end
 
   def customer_email(find_car_request)
     @find_car_request = find_car_request
-    mail(to: @find_car_request.email, from: SENDGRID_VERIFIED_SENDER, subject: "Solicitud de carro")
+    mail(to: @find_car_request.email, subject: "Solicitud de carro")
   end
 end
