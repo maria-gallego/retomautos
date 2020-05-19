e61611d89876e0947a784497457fab2f372f5d1b
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2020-05-19 14:00:03 +1000

    Changed blog title to Automoviles BMW

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,10 +5,10 @@ class BlogPostsController < ApplicationController
           blog_title: 'Bienvenido al mundo de BMW',
           card_text:  'BMW cuenta con un lenguaje de siglas para identificar cada uno de sus vehículos. Pero, ¿cómo identificarlos? ¿Cuál es la diferencia entre un 120i y un 530d?',
           card_link:  blog_posts_bienvenido_al_mundo_de_bmw_path, # => '/blog_posts/bienvenido_al_mundo_de_bmw'
-          blog_image: 'fotos_tu_carro/bmw_420gc.jpg'
+          blog_image: 'blog_photos/bienvenido_bmw.jpg'
       ),
       BlogPost.new(
-          blog_title: 'Las Series de BMW',
+          blog_title: 'Automóviles BMW',
           card_text: 'Desde el Serie 1 al Serie 8, BMW usa esta nomenclatura para identificar la carrocería de cada uno de sus automóviles.',
           card_link: blog_posts_las_series_de_bmw_path,
           blog_image: 'fotos_tu_carro/bmw_420gc.jpg'
@@ -34,7 +34,7 @@ class BlogPostsController < ApplicationController
       ),
       BlogPost.new(
           blog_title: 'Los deportivos de BMW',
-          card_text: 'BMW cuenta con una línea especial de vehícos deportivos, los cuales hacen parte de la división BMW Motorsport, y utilizan la letra M para diferenciarse del resto de vehículos de la marca.',
+          card_text: 'BMW cuenta con una línea especial de vehículos deportivos, los cuales hacen parte de la división BMW Motorsport, y utilizan la letra M para diferenciarse del resto de vehículos de la marca.',
           card_link: blog_posts_los_deportivos_de_bmw_path,
           blog_image: 'fotos_tu_carro/bmw_420gc.jpg'
       ),
