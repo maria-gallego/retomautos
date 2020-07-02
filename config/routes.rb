b7fc294a8b348b13cf32cfe220b439e446570211
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2020-07-02 20:08:48 +1000

    Changed find car request path name to "encontramos tu carro" Whatsapp bubble installed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'visitors#home'
   get '/financiacion', to: 'visitors#financing'
   get '/sobre-nosotros', to: 'visitors#about_us'
+  get '/encontramos-tu-carro', to: 'find_car_requests#new'
   resources :find_car_requests, only: [:new, :create]
   resources :blog_posts, only: [:index]
 
