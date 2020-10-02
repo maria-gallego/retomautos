2a9e249af7dfa1d7e4dbe270f36e75855718db66
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2020-10-02 11:36:35 +1000

    Fix bug Note class_name

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,4 @@
 class Note < ApplicationRecord
   belongs_to :buy_process
-  belongs_to :author, class_name: User, foreign_key: :user_id
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
 end
