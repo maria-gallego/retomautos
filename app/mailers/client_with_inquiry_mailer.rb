5f49b49f21cf6e5ac62a51f5546e73cbff923f1c
Sergio Rodriguez <se.rodriguez68@gmail.com>
2020-10-03 18:15:27 +1000

    ClientWithInquiryMailer change sender and add gmail receiver

--- app/mailers/client_with_inquiry_mailer.rb
+++ app/mailers/client_with_inquiry_mailer.rb
@@ -1,5 +1,5 @@
 class ClientWithInquiryMailer < ApplicationMailer
-  SENDGRID_VERIFIED_SENDER = 'mariapaulagallegoc@gmail.com'
+  SENDGRID_VERIFIED_SENDER = 'maria.gallego@retomautos.com'
   default from: SENDGRID_VERIFIED_SENDER
 
   def staff_email(salesperson:, client:, buy_process:, buy_process_inquiry:)
@@ -7,6 +7,6 @@ class ClientWithInquiryMailer < ApplicationMailer
     @client = client
     @buy_process = buy_process
     @buy_process_inquiry = buy_process_inquiry
-    mail(to: [SENDGRID_VERIFIED_SENDER, salesperson.email], subject: "Pregunta de Cliente")
+    mail(to: [SENDGRID_VERIFIED_SENDER, "se.rodriguez68@gmail.com", salesperson.email], subject: "Pregunta de Cliente")
   end
 end
