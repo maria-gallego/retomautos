7ae84fd2d003c8ad76e98bae6de70d60e8b49684
Maria Paula Gallego <mariapaulagallegoc@gmail.com>
2021-01-04 17:29:05 +1100

    refine fill_car_intake_with_cars to copy the car's created_at

--- db/data_migration_scripts/20210104135600_fill_car_intake_with_cars.rb
+++ db/data_migration_scripts/20210104135600_fill_car_intake_with_cars.rb
@@ -5,6 +5,6 @@ puts "Attempting to update #{number_of_cars_to_update} cars"
 ActiveRecord::Base.transaction do
   cars_to_update.each_with_index do |car, index|
     puts "Attempting to create car intake for car #{index + 1} / #{number_of_cars_to_update}. car: #{car.id}, registration: #{car.registration}, tu_carro_id: #{car.tu_carro_id}"
-    CarIntake.create!(car: car, tu_carro_id: car.tu_carro_id)
+    CarIntake.create!(car: car, tu_carro_id: car.tu_carro_id, created_at: car.created_at)
   end
 end
